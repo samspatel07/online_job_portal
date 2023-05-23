dc3eefec53d9b928eb6370cafe208a2734590950
samspatel07 <sumitpatel787491@gmail.com>
2023-05-23 12:52:17 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,136 @@
+<footer class="footer-wrapper">
+			
+				<div class="main-footer">
+				
+					<div class="container">
+					
+						<div class="row">
+						
+							<div class="col-sm-12 col-md-9">
+							
+								<div class="row">
+								
+									<div class="col-sm-6 col-md-4">
+									
+										<div class="footer-about-us">
+											<h5 class="footer-title">About DearJob</h5>
+											<p>DearJob is a job portal, online job management system developed by sumit, krunal and rudra for his project in 2023.</p>
+										
+										</div>
+
+									</div>
+									
+									<div class="col-sm-6 col-md-5 mt-30-xs">
+										<h5 class="footer-title">Quick Links</h5>
+										<ul class="footer-menu clearfix">
+											<li><a href="./">Home</a></li>
+											<li><a href="job-list.php">Job List</a></li>
+											<li><a href="employers.php">Employers</a></li>
+											<li><a href="employees.php">Employees</a></li>
+											<li><a href="contact.php">Contact Us</a></li>
+											<li><a href="#">Go to top</a></li>
+
+										</ul>
+									
+									</div>
+
+								</div>
+
+							</div>
+							
+							<div class="col-sm-12 col-md-3 mt-30-sm">
+							
+								<h5 class="footer-title">DearJob Contact</h5>
+								
+								<p>Address : Anand, Gujarat, India.</p>
+								<p>Email : <a href="mailto:dearjob.portal@gmail.com">dearjob.portal@gmail.com</a></p>
+								<p>Phone : <a href="tel:+917874916046">+91 7874916046</a></p>
+								
+
+							</div>
+
+							
+						</div>
+						
+					</div>
+					
+				</div>
+				
+				<div class="bottom-footer">
+				
+					<div class="container">
+					
+						<div class="row">
+						
+							<div class="col-sm-4 col-md-4">
+					
+								<p class="copy-right">&#169; Copyright <?php echo date('Y'); ?> DearJob pvt Ltd.</p>
+								
+							</div>
+							
+							<div class="col-sm-4 col-md-4">
+							
+								<ul class="bottom-footer-menu">
+									<li><a >Developed by Sumit, Krunal, Rudra</a></li>
+								</ul>
+							
+							</div>
+							
+							<div class="col-sm-4 col-md-4">
+								<ul class="bottom-footer-menu for-social">
+									<li><a href="<?php echo "$tw"; ?>"><i class="ri ri-twitter" data-toggle="tooltip" data-placement="top" title="twitter"></i></a></li>
+									<li><a href="<?php echo "$fb"; ?>"><i class="ri ri-facebook" data-toggle="tooltip" data-placement="top" title="facebook"></i></a></li>
+									<li><a href="<?php echo "$ig"; ?>"><i class="ri ri-instagram" data-toggle="tooltip" data-placement="top" title="instagram"></i></a></li>
+								</ul>
+							</div>
+						
+						</div>
+
+					</div>
+					
+				</div>
+			
+			</footer>
+			
+		</div>
+
+
+	</div>
+
+<div id="back-to-top">
+   <a href="#"><i class="ion-ios-arrow-up"></i></a>
+</div>
+
+
+<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-modalmanager.js"></script>
+<script type="text/javascript" src="js/bootstrap-modal.js"></script>
+<script type="text/javascript" src="js/smoothscroll.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.waypoints.min.js"></script>
+<script type="text/javascript" src="js/wow.min.js"></script>
+<script type="text/javascript" src="js/jquery.slicknav.min.js"></script>
+<script type="text/javascript" src="js/jquery.placeholder.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-tokenfield.js"></script>
+<script type="text/javascript" src="js/typeahead.bundle.min.js"></script>
+<script type="text/javascript" src="js/bootstrap3-wysihtml5.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-select.min.js"></script>
+<script type="text/javascript" src="js/jquery-filestyle.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-select.js"></script>
+<script type="text/javascript" src="js/ion.rangeSlider.min.js"></script>
+<script type="text/javascript" src="js/handlebars.min.js"></script>
+<script type="text/javascript" src="js/jquery.countimator.js"></script>
+<script type="text/javascript" src="js/jquery.countimator.wheel.js"></script>
+<script type="text/javascript" src="js/slick.min.js"></script>
+<script type="text/javascript" src="js/easy-ticker.js"></script>
+<script type="text/javascript" src="js/jquery.introLoader.min.js"></script>
+<script type="text/javascript" src="js/jquery.responsivegrid.js"></script>
+<script type="text/javascript" src="js/customs.js"></script>
+
+
+</body>
+
+
+</html>
